ac11eb465d925a034341f42925fb01e5dbe5afc4
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 17:47:10 -0300

    feat: add get all categories router

--- src/routers/categoryRouter.ts
+++ src/routers/categoryRouter.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/', categoryController.create);
 router.put('/', categoryController.update);
 router.delete('/', categoryController.remove);
+router.get('/', categoryController.getAll);
 
 export default router;
