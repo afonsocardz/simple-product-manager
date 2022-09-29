7b3e6e8fd4d7a4a545781418b924802c9291fc70
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 13:54:51 -0300

    feat: add update category service

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,18 +1,32 @@
 import { categoryRepository } from "../repositories/categoryRepository";
-import { TCreateCategoryData } from "../types/categoryTypes";
+import { TCreateCategoryData, TUpdateCategory } from "../types/categoryTypes";
 
 async function create(category: TCreateCategoryData) {
   await isCategoryExists(category);
+  await categoryRepository.create(category);
+}
+
+async function update(updateData: TUpdateCategory, id:number){
+  await findOrFail(id);
+  await categoryRepository.update(id, updateData);
+}
+
+async function findOrFail(id:number) {
+  const category = await categoryRepository.getById(id);
+  if(!category){
+    throw {type: 'not_found', message: 'Categoria não encontrada'};
+  }
+  return category;
 }
 
 async function isCategoryExists(categoryData: TCreateCategoryData) {
   const category = await categoryRepository.getByName(categoryData);
-  if(category){
-    throw {type: 'conflict', message: 'Já existe essa categoria'}
+  if (category) {
+    throw { type: 'conflict', message: 'Já existe essa categoria' }
   }
-  await categoryRepository.create(categoryData);
 }
 
 export const categoryService = {
-  create
+  create,
+  update
 }
