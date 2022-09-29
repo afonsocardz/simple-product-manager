d3e750620596f06aca19a9fac24fec20719bba7b
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 18:05:57 -0300

    feat: add products router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import categoryRouter from './categoryRouter';
+import productRouter from './productRouter';
 
 const router = Router();
 
 router.use('/categories', categoryRouter);
+router.use('/products', productRouter)
 
 export default router;
