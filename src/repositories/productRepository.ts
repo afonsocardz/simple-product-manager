5e381d0baffe7900aa09032d8872cbac69c5d5f0
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 17:59:56 -0300

    feat: add product repository getAll, getById, getByName

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -1,5 +1,18 @@
+import { Product } from "@prisma/client";
 import { prisma } from "../config/database";
 import { TCreateProductData, TUpdateProduct } from "../types/productTypes";
+
+async function getAll (): Promise<Product[]>{
+  return await prisma.product.findMany();
+}
+
+async function getById(id: number): Promise<Product> {
+  return await prisma.product.findFirst({ where: { id } });
+}
+
+async function getByName({name}: TCreateProductData){
+  return await prisma.product.findFirst({where: {name}});
+}
 
 async function create(product: TCreateProductData) {
   await prisma.product.create({
@@ -21,6 +34,9 @@ async function remove(id: number){
 };
 
 export const productRepository = {
+  getAll,
+  getByName,
+  getById,
   create,
   update,
   remove
