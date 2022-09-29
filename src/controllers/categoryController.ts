ed55542f88b494d2148b00ebb4c202c0b3355335
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 17:19:39 -0300

    feat: add create category controller

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,1 +1,12 @@
-//category controller
+import { Request, Response } from "express";
+import { categoryService } from "../services/categoryService";
+
+async function create(req: Request, res: Response) {
+  const category = req.body;
+  await categoryService.create(category);
+  res.sendStatus(201);
+}
+
+export const categoryController = {
+  create
+}
