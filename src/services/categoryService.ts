e02f3699fd2ad511be75265239f2bb70e3d84d03
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 13:48:25 -0300

    feat: add create category service

---
+++ src/services/categoryService.ts
@@ -0,0 +1,18 @@
+import { categoryRepository } from "../repositories/categoryRepository";
+import { TCreateCategoryData } from "../types/categoryTypes";
+
+async function create(category: TCreateCategoryData) {
+  await isCategoryExists(category);
+}
+
+async function isCategoryExists(categoryData: TCreateCategoryData) {
+  const category = await categoryRepository.getByName(categoryData);
+  if(category){
+    throw {type: 'conflict', message: 'Já existe esse produto'}
+  }
+  await categoryRepository.create(categoryData);
+}
+
+export const categoryService = {
+  create
+}
