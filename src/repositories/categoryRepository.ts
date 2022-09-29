cc01947b7aa9c0b5fac5842c4f411978e259e86c
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-29 13:34:54 -0300

    feat: add remove category repository

--- src/repositories/categoryRepository.ts
+++ src/repositories/categoryRepository.ts
@@ -1,20 +1,28 @@
+import { number } from "joi";
 import { prisma } from "../config/database";
 import { TCreateCategoryData, TUpdateCategory } from "../types/categoryTypes";
 
-async function create(category: TCreateCategoryData){
+async function create(category: TCreateCategoryData) {
   await prisma.category.create({
     data: category
   });
 }
 
-async function update(id: number, category: TUpdateCategory){
+async function update(id: number, category: TUpdateCategory) {
   await prisma.category.update({
-    where: {id},
+    where: { id },
     data: category
   })
-}
+};
+
+async function remove(id: number){
+  await prisma.category.delete({
+    where: {id}
+  })
+};
 
 export const categoryRepository = {
   create,
-  update
+  update,
+  remove
 }
